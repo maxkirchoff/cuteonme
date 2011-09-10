1c87117fe719016e73b68e4d807237474065482f
jeremiahlee <mass@jeremiahlee.com>
2011-09-10 15:59:29 -0700

    new files

---
+++ dashboard.php
@@ -0,0 +1,7 @@
+<?php
+require('./template.php');	
+?>
+
+User authed. w00t.
+
+<?php print_r($_SESSION); ?>
--- index.php
+++ index.php
@@ -1,7 +1,1 @@
-<?php
-require('./template.php');	
-?>
-
-User authed. w00t.
-
-<?php print_r($_SESSION); ?>
+You should join us!
