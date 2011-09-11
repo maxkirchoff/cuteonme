43fb40496941db212fc1f5cf4ae64f79cf0f2fdf
Jonathan Strauss <jstrauss@jonathan-strausss-macbook.local>
2011-09-10 21:06:38 -0700

    opinion.php with awe.sm conversion hooks.

--- opinion.php
+++ opinion.php
@@ -3,23 +3,23 @@
 	require('./template-header.php');
 ?>
 
-
 <p>
-Person who shared (sharer)
-<?= $_REQUEST['sharer'] ?>
+<span class="sharer"><?= $_REQUEST['sharer'] ?></span> wants to know: <span class="message">&qout;<?= $_REQUEST['message'] ?>&quot;</span>
 </p>
 
 <p>
-Message:
-<?= $_REQUEST['message'] ?>
+<input type="button" id="yes" value="Yes" onclick="AWESM.convert('goal_1',0);"/>
+&nbsp;
+<input type="button" id="no" value="No" onclick="AWESM.convert('goal_2',0);"/>
 </p>
 
-<p>Page:</p>
+
 <iframe src="<?= $_REQUEST['url'] ?>"></iframe>
 
-<input type="button" id="yes" value="Yes"/>
 
-<input type="button" id="no" value="No" />
+
+
+<script src="http://widgets.awe.sm/v3/widgets.js?key=103dbc7485b55313c91aa29176f8ee2ba3e95fe949c574aa5f2505e26a5bb743"></script>
 
 <?php
 	require('./template-footer.php');
