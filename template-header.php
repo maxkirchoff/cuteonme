d38cddadc62dd38d120dc7a0642f5d7fab1c806b
Bennett <bennett@snowballfactory.com>
2011-09-10 23:39:49 -0700

    added twitter icon url to the session
    - modified access token fetch to only occur if no access token data already exists

--- template-header.php
+++ template-header.php
@@ -4,12 +4,21 @@ session_start();
 require_once('./config.php');
 require_once('./lib/twitteroauth/twitteroauth/twitteroauth.php');
 
-if (!empty($_REQUEST['oauth_token']) && !empty($_REQUEST['oauth_verifier'])) {
+if (!empty($_REQUEST['oauth_token']) && !empty($_REQUEST['oauth_verifier']) && empty($_SESSION['access_token'])) {
 	$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $_SESSION['oauth_token'], $_SESSION['oauth_token_secret']);
 	
 	// Request access tokens from twitter
-	// Save the access tokens. Normally these would be saved in a database for future use.
-	$_SESSION['access_token'] = $connection->getAccessToken($_REQUEST['oauth_verifier']);
+	$accessToken =  $connection->getAccessToken($_REQUEST['oauth_verifier']);
+	
+	// fetch more user details
+	$usersDetails = $connection->get(
+		'users/lookup',
+		array('user_id' => $accessToken['user_id']));
+	$accessToken['profile_image_url'] = $usersDetails[0]->profile_image_url;
+
+	// Save the access tokens. Normally these would be saved in a database for future use.	
+	$_SESSION['access_token'] = $accessToken;
+				
 	
 } elseif (empty($_SESSION['access_token']) && empty($unauthedAllowed)) {
 	// No session and the page requires a session
