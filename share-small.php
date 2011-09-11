00acfb4924dae6fafe3ee14c47e2099ee65da185
Bennett <bennett@snowballfactory.com>
2011-09-11 10:32:55 -0700

    trimmed the chrome extension to be smaller

--- share-small.php
+++ share-small.php
@@ -1,5 +1,29 @@
 <?php
-require('./template-header.php');
+session_start();
+
+require_once('./config.php');
+require_once('./lib/twitteroauth/twitteroauth/twitteroauth.php');
+
+if (!empty($_REQUEST['oauth_token']) && !empty($_REQUEST['oauth_verifier']) && empty($_SESSION['access_token'])) {
+	$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $_SESSION['oauth_token'], $_SESSION['oauth_token_secret']);
+	
+	// Request access tokens from twitter
+	$accessToken =  $connection->getAccessToken($_REQUEST['oauth_verifier']);
+	
+	// fetch more user details
+	$usersDetails = $connection->get(
+		'users/lookup',
+		array('user_id' => $accessToken['user_id']));
+	$accessToken['profile_image_url'] = $usersDetails[0]->profile_image_url;
+
+	// Save the access tokens. Normally these would be saved in a database for future use.	
+	$_SESSION['access_token'] = $accessToken;
+				
+	
+} elseif (empty($_SESSION['access_token']) && empty($unauthedAllowed)) {
+	// No session and the page requires a session
+	header('Location: ./signin.php');
+}
 
 // Setup Twitter Connection
 $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $_SESSION['access_token']['oauth_token'], $_SESSION['access_token']['oauth_token_secret']);
@@ -44,6 +68,18 @@ foreach($friendsBatch as $batch) {
 $url = $_REQUEST['url'];
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title><?= (!empty($title))? $title : 'Cute On Me?' ?></title>
+	<link rel="stylesheet" href="/static/css/screen.css" />
+
+</head>
+<body>
+	<div><div class="container">
+
 
 <form action="share-submit.php" method="post">
 	<h3 class="bottomless">Link</h3>
@@ -80,6 +116,6 @@ $url = $_REQUEST['url'];
 	<p class="right"><input type="submit" value="Ask For Advice" class="button" /></p>
 </form>
 
-<?php
-require('./template-footer.php');	
-?>
+</div></div>
+</body>
+</html>
