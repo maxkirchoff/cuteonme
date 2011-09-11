9526096a63b49b8da4a937351fe5fefbcf889186
Bennett <bennett@snowballfactory.com>
2011-09-10 23:08:10 -0700

    added url metadata and twitter user data to the dashboard object

--- dashboard.php
+++ dashboard.php
@@ -1,6 +1,15 @@
 <?php
 require('./template-header.php');
 
+
+// build oauth object
+$connection = new TwitterOAuth(
+	CONSUMER_KEY,
+	CONSUMER_SECRET,
+	$_SESSION['access_token']['oauth_token'],
+	$_SESSION['access_token']['oauth_token_secret']
+);
+
 // extract values from and session
 $apiKey = API_KEY;
 $sharerUserId = $_SESSION['access_token']['user_id'];
@@ -11,14 +20,14 @@ echo "<br>url called is " .print_r($statsApiCall, true) . "<br>";
 $ch = curl_init($statsApiCall);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $response = curl_exec($ch);
-
 $results = json_decode($response, true);
 
 $urlData = array();
-foreach ($results['groups'] as $originalUrls)
+$uniqueUserIds = array();
+foreach ($results['groups'] as $originalUrl)
 {
 	$users = array();
-	foreach ($originalUrls['pivots'] as $pivot)
+	foreach ($originalUrl['pivots'] as $pivot)
 	{
 		if ($pivot['conversions']['goal_1']['count'] > 0)
 		{
@@ -37,14 +46,51 @@ foreach ($results['groups'] as $originalUrls)
 		$users[$pivot['tag']] = array(
 				'user_id' => $pivot['tag'],
 				'response' => $userResponse);
+		$uniqueUserIds[$pivot['tag']] = $pivot['tag'];
 	}
 
-	$urlData[$originalUrls['original_url']] = array(
-			'url' => $originalUrls['original_url'],
+	$urlData[$originalUrl['original_url']] = array(
+			'url' => $originalUrl['original_url'],
 			'users' => $users);
 } 
 
-echo "User data is " . print_r($urlData, true);
+// fetch original url metadata
+$statsApiCall = "http://api.awe.sm/stats/range.json?v=3&key={$apiKey}&group_by=original_url&with_metadata=true&user_id={$sharerUserId}";
+echo "<br>url called is " .print_r($statsApiCall, true) . "<br>";
+$ch = curl_init($statsApiCall);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($ch);
+$results = json_decode($response, true);
+
+$urlMetadata = array();
+foreach ($results['groups'] as $originalUrl)
+{
+	$urlMetadata[$originalUrl['original_url']] = array(
+			'title' => $originalUrl['metadata']['title'],
+			'icon_url' => $originalUrl['metadata']['icon_url']);
+}
+
+// fetch user information
+$friendsDetails = $connection->get(
+	'users/lookup',
+	array(
+		'user_id' => implode(',', array_values($uniqueUserIds))
+	)
+);
+
+// arrange details in a fetchable fashion
+$friendsData = array();
+foreach($friendsDetails as $friendDetails) {
+	$friendsData[$friendDetails->id] = array(
+			'id' => 				$friendDetails->id,
+			'profile_image_url' =>	$friendDetails->profile_image_url,
+			'name' =>				$friendDetails->name,
+			'screen_name' =>		$friendDetails->screen_name);
+}
+
+echo "friends array is " . print_r($friendsData, true);
+echo "Url data is " . print_r($urlData, true);
+echo "Url metdata is " . print_r($urlMetadata, true);
 
 ?>
 
