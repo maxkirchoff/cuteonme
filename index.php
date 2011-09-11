95af5a9fb2a0557080cacd20d5df265f1193ac16
jeremiahlee <mass@jeremiahlee.com>
2011-09-10 22:46:18 -0700

    Styling index

--- index.php
+++ index.php
@@ -4,7 +4,15 @@
 ?>
 
 <h1>Cute On Me?</h1>
-<h2>Quick Fashion Advice From Your Friends</h2>
+<h2><span>Quick Fashion Advice From Your Friends</span></h2>
+
+<p><strong>Cute On Me</strong> is quick fashion advice from your friends.</p>
+
+<p>When you're trying to decide what to buy, let your social network help you make up your mind.</p>
+
+<p>Share what you're looking to buy and collect feedback from the people who know you best.</p>
+
+<p class="center h3">To begin <a href="signin-redirect.php"><img src="/static/img/darker.png" alt="Sign in with Twitter"/></a></p>
 
 <?php
 	require('./template-footer.php');
